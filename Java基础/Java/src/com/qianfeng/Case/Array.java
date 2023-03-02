2aed277f78caf21696c2ddb47b409379a76630fc
linyun <471749311@qq.com>
2023-03-02 20:21:32 +0800

    gitee First

--- Java基础/Java/src/com/qianfeng/Case/Array.java
+++ Java基础/Java/src/com/qianfeng/Case/Array.java
@@ -10,13 +10,15 @@ public class Array {
         int[] ints = new int[50];
 
         for (int i = 0; i < ints.length; i++) {
-            ints[i]= r.nextInt(10)*10;
+            ints[i] = r.nextInt(10) * 10;
         }
         System.out.println(Arrays.toString(ints));
         System.out.println(789);
         System.out.println("push" + 1);
-        System.out.println(789+"linyun");
+        System.out.println(789 + "linyun");
         System.out.println("琳韵");
-         System.out.println("码云——pull");
+        System.out.println("码云——pull");
+        System.out.println("码云——push");
+
     }
 }
