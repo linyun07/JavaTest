308ab78b19e30cba397924343ea1524e9efdcb86
linyun <471749311@qq.com>
2023-03-02 18:19:26 +0800

    Initial commit

---
+++ Java基础/Java/src/com/qianfeng/Case/Array.java
@@ -0,0 +1,18 @@
+package com.qianfeng.Case;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Array {
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        int[] ints = new int[50];
+
+        for (int i = 0; i < ints.length; i++) {
+            ints[i]= r.nextInt(10)*10;;
+        }
+        System.out.println(Arrays.toString(ints));
+        System.out.println("aaaa");
+    }
+}
